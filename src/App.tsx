a1945d526491efc8ea52ec798f98c87614fb7f59
Aaron Pavlick <apavlick@yext.com>
2022-03-16 11:17:40 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { AnswersHeadlessProvider } from '@yext/answers-headless-react';
+import { SearchBar } from '@yext/answers-react-components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AnswersHeadlessProvider
+        apiKey="a1a498f3b492b925ab84f69d27fedf9c"
+        experienceKey="concerts-finder"
+        locale="en"
+        verticalKey="events"
+        endpoints={{
+          universalSearch:
+            'https://liveapi-sandbox.yext.com/v2/accounts/me/answers/query?someparam=blah',
+          verticalSearch: 'https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/query',
+          questionSubmission: 'https://liveapi-sandbox.yext.com/v2/accounts/me/createQuestion',
+          status: 'https://answersstatus.pagescdn.com',
+          universalAutocomplete:
+            'https://liveapi-sandbox.yext.com/v2/accounts/me/answers/autocomplete',
+          verticalAutocomplete:
+            'https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/autocomplete',
+          filterSearch: 'https://liveapi-sandbox.yext.com/v2/accounts/me/answers/filtersearch',
+        }}
+      >
+        <div className="m-6">
+          <SearchBar />
+        </div>
+      </AnswersHeadlessProvider>
     </div>
   );
 }
